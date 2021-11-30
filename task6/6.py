e03021dc736db217df0c64af80eb1c811524a631
Anna <anna-bogacheva86@yandex.ru>
2021-11-30 16:11:21 +0500

    bug fixed

--- task6/6.py
+++ task6/6.py
@@ -1,7 +1,8 @@
 a, b = int(input('Введите км: ')), int(input('Введите км: '))
-counter = 0
+counter = 1
+print(f'{counter}' + '-й день:', f'{a:.2f}')
 while a < b:
+    a = (a * 0.1) + a
     counter += 1
     print(f'{counter}' + '-й день:', f'{a:.2f}')
-    a = (a * 0.1) + a
 print('На', f'{counter}' + '-й день спортсмен достиг результата, не менее', f'{a:.0f}', 'км')
